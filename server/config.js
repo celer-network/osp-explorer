913ff3cd12043ec23736fded3118466f08f869a2
Xuefeng Zhu <xzhu15@illinois.edu>
2020-05-06 21:47:53 -0700

    add heroku

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 8000,
+  port: process.env.PORT || 8000,
   database: "./db.json",
   ethInstance: "wss://ropsten.infura.io/ws/v3/ce581be62b43483b8627f4f9f2ad40d6",
   ledgerContract: "0x4b7a6ee1128ff88b21cc4c6359164b21d671fa31",
